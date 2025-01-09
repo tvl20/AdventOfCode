07570850f61d84f2f35ef89f6113d5e6ca03cca3
TVL-Laptop <teunvanlaarhoven@gmail.com>
2025-01-09 15:41:15 +0100

    added project for day11

---
+++ Day11/Default/DayReader.cs
@@ -0,0 +1,8 @@
+﻿public static class DayReader
+{
+    public static StreamReader GetInputReader()
+    {
+        string inputPath = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent}/Default/input.txt";
+        return new StreamReader(inputPath);
+    }
+}
